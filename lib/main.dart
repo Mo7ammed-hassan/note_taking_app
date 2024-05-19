4e85cb647d29937518b64e2790d40ffabbfc5b7d
mohamedhassan <mohamed7github@gmail.com>
2024-05-19 04:42:28 +0300

    create SplashView

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_taking_app/core/utils/app_colors.dart';
+import 'package:note_taking_app/features/splash/splash_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.cardBodyColor),
         useMaterial3: true,
       ),
-      //home: ,
+      home: const SplashView(),
     );
   }
 }

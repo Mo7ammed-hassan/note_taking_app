52e385e7f2bd9e322299bddb2fa7e24a91839496
mohamedhassan <mohamed7github@gmail.com>
2024-05-19 04:00:48 +0300

    update app colors and create app styles file

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -6,6 +6,9 @@ abstract class AppColors{
   static  Color secoundryColor = const Color(0xff090A0A);
   static  Color accentColor = const Color(0xff836CFB);
   static  Color cardHeadColor = const Color(0xff2A2251);
-  static  Color cardBodyColor = const Color(0xff000000);
+  static  Color cardBodyColor = const Color(0xff72777A);
   static  Color noteBodyColor = const Color(0xff2A2251);
+  static  Color circleBackgroundColor = const Color(0xffE7E7FF);
+  static  Color scaffoldColor = const Color(0xffF2F4F5);
+  static  Color splashColor = const Color(0xffFFFFFF);
 }
---
+++ lib/core/utils/app_text_styls.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+abstract class AppTextStyles{
+  static const splashTextStyle = TextStyle();
+}

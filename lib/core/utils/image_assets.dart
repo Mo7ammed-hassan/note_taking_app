d1cf4c750634f686e77713c1afe2b58c7f91d0eb
mohamedhassan <mohamed7github@gmail.com>
2024-05-19 03:42:04 +0300

    generate image assets path

---
+++ lib/core/utils/image_assets.dart
@@ -0,0 +1,64 @@
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAcademic
+  /// assets/images/academic.svg
+  static const String imagesAcademic = "assets/images/academic.svg";
+
+  /// Assets for imagesAllert
+  /// assets/images/allert.svg
+  static const String imagesAllert = "assets/images/allert.svg";
+
+  /// Assets for imagesBlack
+  /// assets/images/Black.svg
+  static const String imagesBlack = "assets/images/Black.svg";
+
+  /// Assets for imagesCheakednote
+  /// assets/images/cheakednote.svg
+  static const String imagesCheakednote = "assets/images/cheakednote.svg";
+
+  /// Assets for imagesDiary
+  /// assets/images/diary.svg
+  static const String imagesDiary = "assets/images/diary.svg";
+
+  /// Assets for imagesEyeShow
+  /// assets/images/eyeShow.svg
+  static const String imagesEyeShow = "assets/images/eyeShow.svg";
+
+  /// Assets for imagesLamp
+  /// assets/images/lamp.svg
+  static const String imagesLamp = "assets/images/lamp.svg";
+
+  /// Assets for imagesNote
+  /// assets/images/note.svg
+  static const String imagesNote = "assets/images/note.svg";
+
+  /// Assets for imagesOthers
+  /// assets/images/others.svg
+  static const String imagesOthers = "assets/images/others.svg";
+
+  /// Assets for imagesPersonal
+  /// assets/images/personal.svg
+  static const String imagesPersonal = "assets/images/personal.svg";
+
+  /// Assets for imagesPieChart
+  /// assets/images/pieChart.svg
+  static const String imagesPieChart = "assets/images/pieChart.svg";
+
+  /// Assets for imagesSearch
+  /// assets/images/search.svg
+  static const String imagesSearch = "assets/images/search.svg";
+
+  /// Assets for imagesSettings
+  /// assets/images/settings.svg
+  static const String imagesSettings = "assets/images/settings.svg";
+
+  /// Assets for imagesSplashfram
+  /// assets/images/splashfram.svg
+  static const String imagesSplashfram = "assets/images/splashfram.svg";
+
+  /// Assets for imagesWork
+  /// assets/images/work.svg
+  static const String imagesWork = "assets/images/work.svg";
+}
+

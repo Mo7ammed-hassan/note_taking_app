9da17bbcf79ff007e8b93d1c4bf743f679a839fc
mohamedhassan <mohamed7github@gmail.com>
2024-05-19 03:53:39 +0300

    add app colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors{
+  static  Color primaryColor = const Color(0xff6B4EFF);
+  static  Color mainColor = const Color(0xff292150);
+  static  Color secoundryColor = const Color(0xff090A0A);
+  static  Color accentColor = const Color(0xff836CFB);
+  static  Color cardHeadColor = const Color(0xff2A2251);
+  static  Color cardBodyColor = const Color(0xff000000);
+  static  Color noteBodyColor = const Color(0xff2A2251);
+}

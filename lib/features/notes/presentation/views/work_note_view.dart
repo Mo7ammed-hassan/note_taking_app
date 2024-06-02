a28e48c78ac6c7e59f5ae8dcd68121c1b9ccdc3a
mohamedhassan <mohamed7github@gmail.com>
2024-06-02 12:56:34 +0300

    create work note view

---
+++ lib/features/notes/presentation/views/work_note_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WorkNoteView extends StatelessWidget {
+  const WorkNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}

419283c21f269e9d1410e61fac82895abdfeddf3
mohamedhassan <mohamed7github@gmail.com>
2024-06-14 16:24:00 +0300

    connect app with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:note_taking_app/core/helper/route.dart';
 import 'package:note_taking_app/core/utils/app_colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 

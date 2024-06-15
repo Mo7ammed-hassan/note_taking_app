0331d95eb57b7a30ae162f386b985009c5841044
mohamedhassan <mohamed7github@gmail.com>
2024-06-15 16:34:09 +0300

    add app checked package

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,17 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:note_taking_app/features/auth/domain/use_cases/sign_in.dart';
 import 'package:note_taking_app/features/auth/domain/use_cases/sign_up.dart';
 import 'package:note_taking_app/features/auth/persentation/manager/cubit/auth_cubit.dart';
-import 'firebase_options.dart';
+// Import the firebase_app_check plugin
+import 'package:firebase_app_check/firebase_app_check.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+  await Firebase.initializeApp();
+  await FirebaseAppCheck.instance.activate(
+    webProvider: ReCaptchaV3Provider('recaptcha-v3-site-key'),
+    androidProvider: AndroidProvider.debug,
+    appleProvider: AppleProvider.appAttest,
   );
-  setupDependencies();
   runApp(const MyApp());
 }
 

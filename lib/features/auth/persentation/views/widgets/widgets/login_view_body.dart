0bcd32510c7f9dd9b54548b78c655634ab2e5ce5
mohamedhassan <mohamed7github@gmail.com>
2024-06-15 17:02:06 +0300

    change loginForm to login state

--- lib/features/auth/persentation/views/widgets/widgets/login_view_body.dart
+++ lib/features/auth/persentation/views/widgets/widgets/login_view_body.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:note_taking_app/features/auth/persentation/views/widgets/widgets/login_form.dart';
+import 'package:note_taking_app/features/auth/persentation/views/widgets/widgets/login_state.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
@@ -17,7 +17,7 @@ class LoginViewBody extends StatelessWidget {
           ),
           SliverFillRemaining(
             hasScrollBody: false,
-            child: LoginForm(),
+            child: LoginState(),
           ),
         ],
       ),

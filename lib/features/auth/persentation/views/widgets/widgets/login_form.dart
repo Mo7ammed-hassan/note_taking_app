c3b2cf8c97fca8ded66182444079492cd9fc39c9
mohamedhassan <mohamed7github@gmail.com>
2024-06-16 17:45:23 +0300

    ..

--- lib/features/auth/persentation/views/widgets/widgets/login_form.dart
+++ lib/features/auth/persentation/views/widgets/widgets/login_form.dart
@@ -1,7 +1,5 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 import 'package:note_taking_app/core/utils/app_colors.dart';
 import 'package:note_taking_app/core/utils/app_text_styles.dart';
 import 'package:note_taking_app/core/utils/widgets/custom_buttom.dart';
--- lib/features/auth/persentation/views/widgets/widgets/login_state.dart
+++ lib/features/auth/persentation/views/widgets/widgets/login_state.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
 import 'package:note_taking_app/core/utils/functions/build_show_snak_bar.dart';
 import 'package:note_taking_app/features/auth/persentation/manager/cubit/auth_cubit.dart';
 import 'package:note_taking_app/features/auth/persentation/views/widgets/widgets/login_form.dart';

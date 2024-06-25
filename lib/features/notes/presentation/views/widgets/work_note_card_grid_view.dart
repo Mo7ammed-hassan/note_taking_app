396d656ddc62c8faf4694efd0e61e4722a7d5ab8
mohamedhassan <mohamed7github@gmail.com>
2024-06-25 23:23:24 +0300

    refactor code

--- lib/features/notes/presentation/views/widgets/work_note_card_grid_view.dart
+++ lib/features/notes/presentation/views/widgets/work_note_card_grid_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:note_taking_app/core/services/note_services.dart';
-import 'package:note_taking_app/core/utils/constants/boxes.dart';
 import 'package:note_taking_app/features/notes/domain/entites/notes_entity.dart';
 import 'package:note_taking_app/features/notes/presentation/views/widgets/add_new_note_card.dart';
 import 'package:note_taking_app/features/notes/presentation/views/widgets/custom_note_card.dart';
